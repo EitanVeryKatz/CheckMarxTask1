5557e3f4221d1ec331e9bdc67af13397eb978bde
EitanVeryKatz <eitanka@mta.ac.il>
2025-05-08 17:26:35 +0300

    created version fetcher added function to get latest version number of given package

---
+++ CheckMarxTask1/version_fetcher.py
@@ -0,0 +1,24 @@
+
+import requests
+
+def getLatestVersion(i_packageName):
+    """
+    recieves Package name
+    returns Latest version of package 
+    or None if failed to recieve
+    """
+
+
+    url = f"https://pypi.org/pypi/{i_packageName}/json"
+    
+    try:
+        response = requests.get(url, timeout = 10)
+        response.raise_for_status()
+        packageData = response.json()
+        return data["info"]["version"]
+    
+    except requests.exceptions.RequestException as exeptionMessage:
+        print(f"Failed to get Version info for '{i_packageName}'.\
+               Error message: ")
+        return None
+        
