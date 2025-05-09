3e068e8254d8a59a12db4fd15a3d75dbd2dc264f
EitanVeryKatz <eitanka@mta.ac.il>
2025-05-09 11:40:20 +0300

    added support for longer pathways on windows OS

--- CheckMarxTask1/tarExtractor.py
+++ CheckMarxTask1/tarExtractor.py
@@ -1,7 +1,17 @@
 import tarfile
+import os
+import sys
 
 def extractTarball(i_filepath, i_extract_to):
     try:
+        print("began extraction process")
+        limitedPathLengthsOS = ("win32","win64")
+        if sys.platform in limitedPathLengthsOS:
+            i_extract_to = os.path.abspath(i_extract_to)
+            if not i_extract_to.startswith("\\\\?\\"):
+                i_extract_to = f"\\\\?\\{i_extract_to}"
+                
+
         with tarfile.open(i_filepath, "r:gz") as tar:
             tar.extractall(path=i_extract_to)
         print(f"Extracted to {i_extract_to}")
--- CheckMarxTask1/test_fetch.py
+++ CheckMarxTask1/test_fetch.py
@@ -1,22 +1,30 @@
 ﻿
+from re import I
 from version_fetcher import getLatestVersion
 from downloadPackage import download_package
 from tarExtractor import extractTarball
 
-pkg = input("Enter package name: ")
-version = getLatestVersion(pkg)
 
-if version:
-    print(f"Latest version of '{pkg}': {version}")
-    file = download_package(pkg, version)
-    if file:
-        print(f"Downloaded file: {file}")
-        if extractTarball(file, "extracted"):
-            print(f"Extracted to 'extracted' directory.")
+
+def main():
+    pkg = input("Enter package name: ")
+    version = getLatestVersion(pkg)
+
+    if version:
+        print(f"Latest version of '{pkg}': {version}")
+        file = download_package(pkg, version)
+        if file:
+            print(f"Downloaded file: {file}")
+            if extractTarball(file, "extracted"):
+                print(f"Extracted to 'extracted' directory.")
+            else:
+                print("Failed to extract the tarball.")
         else:
-            print("Failed to extract the tarball.")
-    else:
-        print("Failed to download the package.")
+            print("Failed to download the package.")
     
-else:
-    print("Failed to fetch version.")
+    else:
+        print("Failed to fetch version.")
+
+
+if __name__ == "__main__":
+    main()
